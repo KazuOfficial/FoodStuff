10891598bb8077dab0db45bc45a63313c24e9413
KazuGod <kazuzmafii@gmail.com>
2020-06-07 13:53:54 +0200

    Shop layout & JQuery

--- Scripts/removeClass.js
+++ Scripts/removeClass.js
@@ -1,4 +1,9 @@
 ﻿function removeClass(divname, classname) {
     var element = document.getElementById(divname);
     element.classList.remove(classname);
+    var secondelement = document.getElementById("foodmenu");
+    element.classList.remove("col-lg-12");
+    secondelement.className = "col-lg-6";
+    $("#shop").animate({ top: "+=15" }, 400);
+    $("#shop").animate({ top: "-=15" }, 300);
 }
