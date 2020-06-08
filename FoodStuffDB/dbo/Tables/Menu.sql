03d58c6598e763606932a45efc4c573b5d21f825
KazuGod <kazuzmafii@gmail.com>
2020-06-08 21:22:04 +0200

    Updates

--- FoodStuffDB/dbo/Tables/Menu.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Menu]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(50) NOT NULL, 
-    [Description] VARCHAR(50) NOT NULL, 
-    [Price] FLOAT NOT NULL, 
-    [Category] VARCHAR(50) NOT NULL
-)
