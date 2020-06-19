60e02c31f60cd3d535a05b3bd62b0ba38bf53374
KazuGod <kazuzmafii@gmail.com>
2020-06-19 11:20:12 +0200

    Mobile support

--- Scripts/UIManager.js
+++ Scripts/UIManager.js
@@ -1,5 +1,5 @@
 ﻿function Firstback() {
-    addClass('shop', 'hidden col-lg-6 pull-right jumbotron');
+    addClass('shop', 'hidden col-lg-6 col-xs-12 pull-right jumbotron');
     removeClass('foodmenu', 'col-lg-6');
     addClass('foodmenu', 'col-lg-12');
 }
