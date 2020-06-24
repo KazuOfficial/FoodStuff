ee09ae1a335d2cd99f49c256f6960d7d80e55727
KazuGod <kazuzmafii@gmail.com>
2020-06-24 19:53:00 +0200

    Order index

--- Scripts/shoppingBasket.js
+++ Scripts/shoppingBasket.js
@@ -6,6 +6,8 @@ var names = [];
 var prices = [];
 var counter = 0;
 
+var foodcounter = 1;
+
 function removeClass(divname, classname) {
 
     var element = document.getElementById(divname);
@@ -42,7 +44,6 @@ function fullPrice() {
         return a + b;
     }, 0);
 
-    //fullP = parseFloat(fullP).toFixed(2) - 2.0;
     fullP = parseFloat(fullP).toFixed(2);
 
     document.getElementById("fullp").innerHTML = "$" + fullP;
@@ -54,13 +55,6 @@ function removeItem(atd, datd) {
 
     names.splice(names.indexOf(atd), 1);
     prices.splice(prices.indexOf(datd), 1);
-
-    /*console.log("[RemoveItem] Removed " + atd);
-    console.log("[RemoveItem] showing 'names' list ");
-    console.log(names);
-    console.log("[RemoveItem] Removed " + datd);
-    console.log("[RemoveItem] showing 'prices' list ");
-    console.log(prices);*/
 
     for (i = 0; i < 4; i++) {
         var stuff = document.getElementById(atd);
@@ -76,10 +70,12 @@ function removeItem(atd, datd) {
     }
 
     var presubtract = parseFloat(datd).toFixed(2);
-    /*console.log('subtract: ' + subtract);
-    console.log('presubtract: ' + presubtract);
-    console.log('datd: ' + datd);*/
     fullPrice();
+}
+
+function removeRow(atd) {
+    var stuff = document.getElementById(atd);
+    stuff.remove();
 }
 
 function addRow(at, dat) {
@@ -99,7 +95,7 @@ function addRow(at, dat) {
     var binCell = newRow.insertCell(3);
     binCell.setAttribute("id", at);
 
-    var newText = document.createTextNode("x1");
+    var newText = document.createTextNode("x" + foodcounter);
     var newName = document.createTextNode(names[counter]);
     var newPrice = document.createTextNode("$" + prices[counter]);
 
@@ -116,14 +112,6 @@ function addRow(at, dat) {
     priceCell.appendChild(newPrice);
     binCell.appendChild(btntag);
     counter++;
-
-    /*console.log("[addRow] showing 'names'")
-    console.log(names);
-    console.log("[addRow] showing 'prices'")
-    console.log(prices);
-    console.log("[addRow] showing 'counter'")
-    console.log(counter);*/
-    countDuplicates();
 }
 
 function animFade (fadeDiv, value) {
@@ -141,23 +129,23 @@ function animShop() {
     $("#shop").animate({ top: "-=15" }, 300);
 }
 
-//if span z spanid equals 420 then change 420 to CLOSED
-function closedFunction(duh) {
-    var span = document.getElementById(duh);
-    if (span.contains("420AM")) {
-        console.log("kurwamac420");
-    }
-}
-
 function scrollToO(scrolldiv) {
     $('html, body').animate({
         scrollTop: $(scrolldiv).offset().top - 105
     }, 800);
 }
 
-function countDuplicates() {
+function countDuplicates(element) {
     var result = {};
     names.forEach(function (x) { result[x] = (result[x] || 0) + 1; });
+
+    if (result[element] > 1) {
+        for (i = 0; i < 4; i++) {
+            removeRow(element);
+            //foodcounter++;
+        }
+    }
+
     console.log(result);
-    console.log(names);
+    console.log(result.Stopiatka);
 }
