23066b70e72e003468ae6e6178fe12a56fcf0edd
KazuGod <kazuzmafii@gmail.com>
2020-07-21 15:07:46 +0200

    Allow buying products only in specified hours

--- Scripts/Time.js
+++ Scripts/Time.js
@@ -1,21 +1,13 @@
-﻿window.onload = function () {
-    setTimeout(stuff, 5000);
-};
-
-function stuff(openh, closeh, openm=0, closem=60) {
-
-    setTimeout(stuff, 5000);
-
-    openh = 7;
-    closeh = 21;
-
+﻿function stuff(openm = 0, closem = 60) {
     var today = new Date();
     //Delete the line below
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var hours = today.getHours();
     var minutes = today.getMinutes();
 
-    //var getH = getHours();
+    var getH = getHours();
+    var openh = getH[0];
+    var closeh = getH[1];
 
     if (hours >= openh && hours < closeh && minutes >= openm && minutes < closem) {
         console.log("open");
@@ -24,24 +16,6 @@ function stuff(openh, closeh, openm=0, closem=60) {
         console.log("closed");
     }
 
-    console.log(time);
-    setTimeout(stuff, 5000);
-}
-
-function getHours(element) {
-
-    var d = document.getElementById(element);
-    d = d.innerHTML;
-
-    var openh = d.split("").slice(0)[0];
-    var closeh = d.split("").slice(-3)[0];
-
     console.log(openh);
     console.log(closeh);
-
-    return [openh, closeh];
-
-    //var getH = getHours();
-    //var openh = getH[0];
-    //var closeh = getH[1];
 }
