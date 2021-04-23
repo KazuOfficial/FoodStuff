af71ece7b890365c02f34db5dc05887c24fc25ae
KazuOfficial <kazuofficial.contact@gmail.com>
2021-04-23 16:40:46 +0200

    Add and wire up Login form to the API

--- FoodStuff-API/FoodStuffDesktop/Bootstrapper.cs
+++ FoodStuff-API/FoodStuffDesktop/Bootstrapper.cs
@@ -1,24 +1,52 @@
 ﻿using Caliburn.Micro;
-using FoodStuffDesktop.ViewsModels;
+using FoodStuffDesktop.Helpers;
+using FoodStuffDesktop.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace FoodStuffDesktop
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
-        protected override void OnStartup(object sender, StartupEventArgs e)
+        protected override void Configure()
         {
-            DisplayRootViewFor<ShellViewModel>();
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) => DisplayRootViewFor<ShellViewModel>();
+
+        protected override object GetInstance(Type service, string key) => _container.GetInstance(service, key);
+
+        protected override IEnumerable<object> GetAllInstances(Type service) => _container.GetAllInstances(service);
+
+        protected override void BuildUp(object instance) => _container.BuildUp(instance);
     }
 }
---
+++ FoodStuff-API/FoodStuffDesktop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Caliburn.Micro;
+using FoodStuffDesktop.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodStuffDesktop.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private IAPIHelper _apiHelper;
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get 
+            {
+                bool output = false;
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
