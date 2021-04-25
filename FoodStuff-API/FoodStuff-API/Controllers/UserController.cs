975782f63d4fe8cbf021cff27db834bea161eccf
KazuOfficial <kazuofficial.contact@gmail.com>
2021-04-25 21:40:21 +0200

    Add Database Connection and FoodStuffMobile Project

---
+++ FoodStuff-API/FoodStuff-API/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using FoodStuff_API.Library.DataAccess;
+using FoodStuff_API.Library.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace FoodStuff_API.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+
+        } 
+    }
+}
