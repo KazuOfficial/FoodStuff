0a45542493157c43ac80a923ec7fc1457a451775
KazuOfficial <kazuofficial.contact@gmail.com>
2021-05-17 16:22:32 +0200

    Upgrade API to .NET Core

---
+++ FoodStuff-API/FoodStuffAPI/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using FoodStuff_API.Library.DataAccess;
+using FoodStuff_API.Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodStuffAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class InventoryController : ControllerBase
+    {
+        [Authorize(Roles = "Manager,Admin")]
+        public List<InventoryModel> Get()
+        {
+            InventoryData data = new InventoryData();
+
+            return data.GetInventory();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public void Post(InventoryModel item)
+        {
+            InventoryData data = new InventoryData();
+            data.SaveInventoryRecord(item);
+        }
+    }
+}
